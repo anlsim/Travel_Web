e44c61c40add5834f3973306d052268533d521ff
anlsim <anlsimental@gmail.com>
2021-02-06 17:14:03 -0800

    Updating MAP functionality
    User can click on state and it will be redirected to the update for that State

--- travel_website/src/NavBar.js
+++ travel_website/src/NavBar.js
@@ -7,7 +7,7 @@ import Navbar from 'react-bootstrap/Navbar'
 const NavBar = () => (
 <>
 <Navbar bg="light" variant="light">
-    <Navbar.Brand href="/">Navbar</Navbar.Brand>
+    <Navbar.Brand href="/">Travel Blog</Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="/">Home</Nav.Link>
       <Nav.Link href="/about">About</Nav.Link>
--- travel_website/src/components/Map.js
+++ travel_website/src/components/Map.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 import USAMap from "react-usa-map";
+import Modal from 'react-bootstrap/Modal';
+
+
 
 class Map extends Component {
-
+  
   mapHandler = (event) => {
-    var stateName = event.target.dataset.name;
-    alert(stateName);
+    let stateName = event.target.dataset.name;
+
+    
+    console.log(stateName);
+   
+    window.location.href = '/update/' + (stateName);
+  //   <Modal.Header closeButton>
+  //   <Modal.Title>{stateName}</Modal.Title>
+  // </Modal.Header>
   };
   
   render() {
     return (
       <div className="App">
         <USAMap onClick={this.mapHandler} />
+
       </div>
+  
     );
   }
+  
 }
 
 export default Map;
