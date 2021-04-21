892219abf53630c6cbd914c031fe4f3bd5beeaf9
anlsim <anlsimental@gmail.com>
2021-04-20 20:44:48 -0700

    creating commentList component

--- travel_website/src/components/UpdateEntry/UpdateEntry.js
+++ travel_website/src/components/UpdateEntry/UpdateEntry.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,  {useState, useEffect} from 'react';
 import updateContent from '../Updates-content/Update-content';
 import UpdatesList from '../../pages/BlogPage/BlogPage';
 import NotFoundPage from '../../pages/ErrorsPage/NotFoundPage';
@@ -6,6 +6,17 @@ import NotFoundPage from '../../pages/ErrorsPage/NotFoundPage';
 const UpdatePage = ({match}) => {
     const stateName = match.params.stateName;
     const update = updateContent.find(update => update.stateName === stateName);
+    const [updateInfo, setArticleInfo] = useState({likes: 0, comments: []});
+    useEffect(()=>{
+        const fetchData = async() => {
+            const result = await fetch(`/api/articles/${update}`);
+            const body = await result.json();
+            console.log(body);
+            setArticleInfo(body);
+        }
+        fetchData();
+        setArticleInfo({likes: 3}, [update]);
+    });
     if(!update) return <NotFoundPage />
     const otherUpdates = updateContent.filter(update => update.stateName !== stateName);
     return(
@@ -13,6 +24,7 @@ const UpdatePage = ({match}) => {
 
         <h1>{update.stateName}</h1>
         <h2>{update.title} </h2>
+        <p>This Post has been like {updateInfo.likes} times</p>
         {update.content.map((paragraph, key)=>(
             <p key ={key}> {paragraph}</p>
         ))}
