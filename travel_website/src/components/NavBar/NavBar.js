fa4e3d8e598a7da45c3abda0ce934be492b198d6
anlsim <anlsimental@gmail.com>
2021-05-09 16:48:52 -0700

    Updating Dependencies and Adding Properly bootstrap

--- travel_website/src/components/NavBar/NavBar.js
+++ travel_website/src/components/NavBar/NavBar.js
@@ -4,18 +4,15 @@ import {Link} from 'react-router-dom';
 
 const NavBar = () => (
 <>
-
-
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-  <Link className="navbar-brand" to="/">TRAVEL BLOG</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <Link className="navbar-brand" to="/">TRAVEL BLOG</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-
-        <li className="nav-item">
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <li className="nav-item">
             <Link className="nav-link" to="/blog">BLOG</Link>
         </li>
         <li className="nav-item">
@@ -32,7 +29,8 @@ const NavBar = () => (
     </div>
   </div>
 </nav>
+</>
 
-    </>
+
 );
 export default NavBar;
--- travel_website/src/index.js
+++ travel_website/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './custom.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
 
 ReactDOM.render(
