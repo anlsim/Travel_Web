49dd6115407400ae23b347ae80395e5ab89865d6
anlsim <anlsimental@gmail.com>
2021-05-09 17:46:27 -0700

    Updating PostList Styles

--- travel_website/src/components/PostList/PostData.js
+++ travel_website/src/components/PostList/PostData.js
@@ -3,7 +3,7 @@ const PostData = [
        
         date: '04202121',
         stateName: 'CO',
-        title: 'The First Week',
+        title: 'The First in List Lorem ipsum dolor sit amet',
         imgUrl: 'https://picsum.photos/200',
         content: [`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nihil est enim, de quo minus dubitari possit, quam et honesta expetenda per se et eodem modo turpia per se esse fugienda. Ita, quem ad modum in senatu semper est aliquis, qui interpretem postulet, sic, isti nobis cum interprete audiendi sunt. Tanta vis admonitionis inest in locis; Duo Reges: constructio interrete. Indicant pueri, in quibus ut in speculis natura cernitur. Hinc ceteri particulas arripere conati suam quisque videro voluit afferre sententiam. Urgent tamen et nihil remittunt.`]
     },
@@ -18,7 +18,7 @@ const PostData = [
     {
         date: '04212021',
         stateName: 'VT',
-        title: 'First 2 Weeks',
+        title: 'Third in List',
         imgUrl: 'https://picsum.photos/200',
         content: [`Ita fit cum gravior, tum etiam splendidior oratio. Huic mori optimum esse propter desperationem sapientiae, illi propter spem vivere.`,
          `Haeret in salebra. Ita est quoddam commune officium sapientis et insipientis, ex quo efficitur versari in iis, quae media dicamus. Atque ab his initiis profecti omnium virtutum et originem et progressionem persecuti sunt.`,
@@ -27,24 +27,33 @@ const PostData = [
     {
         date: '11182021',
         stateName: 'NY',
-        title: 'Crazy weather',
+        title: ' 4 Crazy weather',
         imgUrl: 'https://picsum.photos/200',
         content: [`Quamquam non negatis nos intellegere quid sit voluptas, sed quid ille dicat. Itaque e contrario moderati aequabilesque habitus, affectiones ususque corporis apti esse ad naturam videntur. Nec enim figura corporis nec ratio excellens ingenii humani significat ad unam hanc rem natum hominem, ut frueretur voluptatibus. Claudii libidini, qui tum erat summo ne imperio, dederetur. At tu eadem ista dic in iudicio aut, si coronam times, dic in senatu. Itaque homo in primis ingenuus et gravis, dignus illa familiaritate Scipionis et Laelii, Panaetius, cum ad Q. Laboro autem non sine causa; Invidiosum nomen est, infame, suspectum.`]
     }, 
     {
         date: '11182020',
         stateName: 'NY',
-        title: 'Best Week',
+        title: '5 Best Week',
         imgUrl: 'https://picsum.photos/200',
         content: [`Quamquam non negatis nos intellegere quid sit voluptas, sed quid ille dicat. Itaque e contrario moderati aequabilesque habitus, affectiones ususque corporis apti esse ad naturam videntur. Nec enim figura corporis nec ratio excellens ingenii humani significat ad unam hanc rem natum hominem, ut frueretur voluptatibus. Claudii libidini, qui tum erat summo ne imperio, dederetur. At tu eadem ista dic in iudicio aut, si coronam times, dic in senatu. Itaque homo in primis ingenuus et gravis, dignus illa familiaritate Scipionis et Laelii, Panaetius, cum ad Q. Laboro autem non sine causa; Invidiosum nomen est, infame, suspectum.`]
     }, 
     {
         date: '106182021',
         stateName: 'NY',
-        title: 'A Great Restaurant',
+        title: ' 6 A Great Restaurant',
         imgUrl: 'https://picsum.photos/200',
         content: [`Quamquam non negatis nos intellegere quid sit voluptas, sed quid ille dicat. Itaque e contrario moderati aequabilesque habitus, affectiones ususque corporis apti esse ad naturam videntur. Nec enim figura corporis nec ratio excellens ingenii humani significat ad unam hanc rem natum hominem, ut frueretur voluptatibus. Claudii libidini, qui tum erat summo ne imperio, dederetur. At tu eadem ista dic in iudicio aut, si coronam times, dic in senatu. Itaque homo in primis ingenuus et gravis, dignus illa familiaritate Scipionis et Laelii, Panaetius, cum ad Q. Laboro autem non sine causa; Invidiosum nomen est, infame, suspectum.`]
     }, 
+    {
+        date: '050920212',
+        stateName: 'NY',
+        title: 'I am the LAST in the List LAST in the List',
+        imgUrl: 'https://picsum.photos/200',
+        content: [`Quamquam non negatis nos intellegere quid sit voluptas, sed quid ille dicat. Itaque e contrario moderati aequabilesque habitus, affectiones ususque corporis apti esse ad naturam videntur. Nec enim figura corporis nec ratio excellens ingenii humani significat ad unam hanc rem natum hominem, ut frueretur voluptatibus. Claudii libidini, qui tum erat summo ne imperio, dederetur. At tu eadem ista dic in iudicio aut, si coronam times, dic in senatu. Itaque homo in primis ingenuus et gravis, dignus illa familiaritate Scipionis et Laelii, Panaetius, cum ad Q. Laboro autem non sine causa; Invidiosum nomen est, infame, suspectum.`]
+    }, 
+    
+    
     
 
 
--- travel_website/src/components/PostList/PostList.js
+++ travel_website/src/components/PostList/PostList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link } from 'react-router-dom';
+import "./PostList.scss"
 
 const PostList = ({updates}) => (
     <>
@@ -7,10 +8,15 @@ const PostList = ({updates}) => (
         
         {updates.map((update, id)=>(
             <div className="card" >
-                <div className="card-body">
+                <img src={update.imgUrl} class="card-img-top" alt="..."></img>
+                <div class="card-img-overlay">
                 <Link key = {id} to = {`/post/${update.date}`}>
-                  <h3 className="card-title">{update.title}</h3>
-                  <p>{update.content[0].substring(0, 100)}...</p>  
+                    <div className="bottom">
+                    <p className="card-text"><small>Last updated 3 mins ago</small></p> 
+                    <h3 className="card-title">{update.title}</h3>
+                    </div>
+                  {/* <p className="card-text">{update.content[0].substring(0, 100)}...</p> */}
+                   
                 </Link>
                 </div>
             </div>   
