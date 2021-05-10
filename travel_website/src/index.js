cc13cb861e96beaca075d5a6db02bdb2c47ade90
anlsim <anlsimental@gmail.com>
2021-05-09 19:52:18 -0700

    adding styles to navbar

--- travel_website/src/index.js
+++ travel_website/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './custom.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
