e5a9964e0549b3eee8e8ac12955af02782f7755b
anlsim <anlsimental@gmail.com>
2021-05-11 21:01:35 -0700

    Adding styles for blog page

--- travel_website/src/pages/BlogPage/BlogPage.js
+++ travel_website/src/pages/BlogPage/BlogPage.js
@@ -5,13 +5,16 @@ import Hero from '../../components/Hero/Hero';
 import SideMenu from '../../components/SideMenu/SideMenu';
 import PostList from '../../components/PostList/PostList';
 import PostData from '../../components/PostList/PostData';
+import './BlogPage.scss'
 
 const BlogPage = () => (
     <>
     <Hero title = {'Blog'}/>
     <SideMenu />
-
+    <div className="blogs">
     <PostList updates = {PostData}/>
+    </div>
+    
     </>
 
 );
