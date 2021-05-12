74b4ca934a87cd2ec3f9432e55570f21c5981bb6
anlsim <anlsimental@gmail.com>
2021-05-11 21:11:25 -0700

    Adding elements to Sidebar

--- travel_website/src/components/SideMenu/SideMenu.js
+++ travel_website/src/components/SideMenu/SideMenu.js
@@ -3,7 +3,11 @@ import React from 'react';
 import './SideMenu.scss';
 const SideMenu = () => (
     <>
-
+    <nav className="sidenav">
+        <a className="nav-link active" aria-current="page" href="/">States</a>
+        <a className="nav-link" href="/">Cities</a>
+        <a className="nav-link" href="/">National Parks</a>
+    </nav>
     </>
 );
 
