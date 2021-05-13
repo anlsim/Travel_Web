db51b482f482a20523a7e633025eb7d5d93c49b9
anlsim <anlsimental@gmail.com>
2021-05-12 20:57:53 -0700

    Adding react-grid-gallery

--- travel_website/src/pages/GalleryPage/Gallery.js
+++ travel_website/src/pages/GalleryPage/Gallery.js
@@ -2,18 +2,24 @@ import React from 'react';
 import Hero from '../../components/Hero/Hero';
 // import GalleryGrid from "./GalleryGrid";
 import GalleryGrid from 'react-grid-gallery';
+import './Gallery.scss';
 const images =[
     { 
         src: "https://picsum.photos/200/300",
         thumbnail: "https://picsum.photos/200/300",
+        margin: "15",
     },
     { 
         src:"https://picsum.photos/400/300",
         thumbnail: "https://picsum.photos/400/300",
+        tags: [{value: "People", title: "People"},
+                   {value: "Industrial", title: "Industrial"}],
+            caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)"
     },
     { 
         src:"https://picsum.photos/400/300",
         thumbnail: "https://picsum.photos/400/300",
+        
     },
     { 
         src: "https://picsum.photos/200/300",
@@ -68,7 +74,10 @@ const images =[
 const GalleryPage = () =>(
     <>
     <Hero title={'Gallery'}/>
-    <GalleryGrid images={images}/>
+    <div className="galleryContainer">
+
+    <GalleryGrid images={images} margin={10} rowHeight={300}/>
+    </div>
     </>
 );
 
