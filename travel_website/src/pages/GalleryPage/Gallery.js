fe78e4e9cbdd3d6cf257fe9df75e858f554ae807
anlsim <anlsimental@gmail.com>
2021-05-12 20:44:48 -0700

    Adding Data for Gallery

--- travel_website/src/pages/GalleryPage/Gallery.js
+++ travel_website/src/pages/GalleryPage/Gallery.js
@@ -1,11 +1,74 @@
 import React from 'react';
 import Hero from '../../components/Hero/Hero';
+// import GalleryGrid from "./GalleryGrid";
+import GalleryGrid from 'react-grid-gallery';
+const images =[
+    { 
+        src: "https://picsum.photos/200/300",
+        thumbnail: "https://picsum.photos/200/300",
+    },
+    { 
+        src:"https://picsum.photos/400/300",
+        thumbnail: "https://picsum.photos/400/300",
+    },
+    { 
+        src:"https://picsum.photos/400/300",
+        thumbnail: "https://picsum.photos/400/300",
+    },
+    { 
+        src: "https://picsum.photos/200/300",
+        thumbnail: "https://picsum.photos/200/300",
+    },
+    { 
+        src:"https://picsum.photos/400/300",
+        thumbnail: "https://picsum.photos/400/300",
+    },
+    { 
+        src:"https://picsum.photos/400/300",
+        thumbnail: "https://picsum.photos/400/300",
+    },
+    { 
+        src: "https://picsum.photos/200/300",
+        thumbnail: "https://picsum.photos/200/300",
+    },
+    { 
+        src:"https://picsum.photos/400/300",
+        thumbnail: "https://picsum.photos/400/300",
+    },
+    { 
+        src:"https://picsum.photos/200/400",
+        thumbnail: "https://picsum.photos/200/400",
+    },
+    { 
+        src:"https://picsum.photos/400/400",
+        thumbnail: "https://picsum.photos/400/400",
+    },
+    { 
+        src:"https://picsum.photos/400/300",
+        thumbnail: "https://picsum.photos/400/300",
+    },
+    { 
+        src:"https://picsum.photos/500/300",
+        thumbnail: "https://picsum.photos/500/300",
+    },
+    { 
+        src: "https://picsum.photos/200/300",
+        thumbnail: "https://picsum.photos/200/300",
+    },
+    { 
+        src:"https://picsum.photos/400/300",
+        thumbnail: "https://picsum.photos/400/300",
+    },
+    { 
+        src:"https://picsum.photos/400/400",
+        thumbnail: "https://picsum.photos/400/400",
+    },
 
+]
 const GalleryPage = () =>(
     <>
     <Hero title={'Gallery'}/>
-    <div>
-    </div>
+    <GalleryGrid images={images}/>
     </>
 );
 
