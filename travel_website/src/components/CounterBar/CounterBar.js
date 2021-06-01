28e3fe326ec757e10415de1ed0d2b016c8676f0d
anlsim <anlsimental@gmail.com>
2021-05-31 17:12:19 -0700

    Fix variable for Counterbar

--- travel_website/src/components/CounterBar/CounterBar.js
+++ travel_website/src/components/CounterBar/CounterBar.js
@@ -1,18 +1,17 @@
 import * as React from 'react';
 
 import './CounterBar.scss';
-const {useState} = React;
-const start = new Date(2021, 4, 28);
-const today = new Date();
-//const daysCounter  = today - start;
+import PostData from "../PostList/PostData";
+
 
 export default function CounterBar() {
-       const [citiesCounter, setCitiesCounter] = useState(10);
-       //const [daysCounter, setDaysCounter] = useState(today - start);
+       const start = new Date(2021, 4, 28);
+       const today = new Date();
        const daysCounter  =  Math.ceil((today - start + 1) / 86400000);
+       const cities = PostData.map(obj => obj.cityName);
        return(     
        <div className="Bar">
-              <h1 id="titleBar">Cities Visited: {citiesCounter}</h1>
+              <h1 id="titleBar">Cities Visited: {cities.length}</h1>
               <h1 id="titleBar">Days on the road: {daysCounter}</h1>
        
        </div>
--- travel_website/src/components/PostList/PostData.js
+++ travel_website/src/components/PostList/PostData.js
@@ -3,7 +3,6 @@ const PostData = [
         date: '05202021',
         stateName: 'WA',
         cityName: 'Spokane',
-        city: 'Spokane',
         title: 'Second Week',
         imgUrl: 'https://picsum.photos/300',
         postImage: [
@@ -14,11 +13,25 @@ const PostData = [
         location: [47.65754562, -117.422810786],
         content: [`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nihil est enim, de quo minus dubitari possit, quam et honesta expetenda per se et eodem modo turpia per se esse fugienda. Ita, quem ad modum in senatu semper est aliquis, qui interpretem postulet, sic, isti nobis cum interprete audiendi sunt. Tanta vis admonitionis inest in locis; Duo Reges: constructio interrete. Indicant pueri, in quibus ut in speculis natura cernitur. Hinc ceteri particulas arripere conati suam quisque videro voluit afferre sententiam. Urgent tamen et nihil remittunt.`]
     },
+    {   
+       
+        date: '04202121',
+        stateName: 'WY',
+        cityName: 'Moran',
+        title: 'Yellowstone National Park',
+        imgUrl: 'https://picsum.photos/200/300',
+        postImage: [
+                'https://c4.staticflickr.com/9/8578/28357117603_97a8233cf5_b.jpg',
+                'https://c6.staticflickr.com/9/8893/28897116141_641b88e342_b.jpg',
+                'https://c1.staticflickr.com/9/8056/28354485944_148d6a5fc1_b.jpg',
+        ],
+        location: [ 44.51933365363961, -110.32951055172983],
+        content: [`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nihil est enim, de quo minus dubitari possit, quam et honesta expetenda per se et eodem modo turpia per se esse fugienda. Ita, quem ad modum in senatu semper est aliquis, qui interpretem postulet, sic, isti nobis cum interprete audiendi sunt. Tanta vis admonitionis inest in locis; Duo Reges: constructio interrete. Indicant pueri, in quibus ut in speculis natura cernitur. Hinc ceteri particulas arripere conati suam quisque videro voluit afferre sententiam. Urgent tamen et nihil remittunt.`]
+    },
     {
         date: '11182020',
         stateName: 'SD',
         cityName: 'Rapid City',
-        city: 'Rapid City',
         title: '5 Best Week',
         imgUrl: 'https://picsum.photos/500',
         postImage: [
@@ -28,7 +41,69 @@ const PostData = [
         ],
         location: [44.11652366, -103.237857],
         content: [`Quamquam non negatis nos intellegere quid sit voluptas, sed quid ille dicat. Itaque e contrario moderati aequabilesque habitus, affectiones ususque corporis apti esse ad naturam videntur. Nec enim figura corporis nec ratio excellens ingenii humani significat ad unam hanc rem natum hominem, ut frueretur voluptatibus. Claudii libidini, qui tum erat summo ne imperio, dederetur. At tu eadem ista dic in iudicio aut, si coronam times, dic in senatu. Itaque homo in primis ingenuus et gravis, dignus illa familiaritate Scipionis et Laelii, Panaetius, cum ad Q. Laboro autem non sine causa; Invidiosum nomen est, infame, suspectum.`]
+    },
+    {
+        date: '05202021',
+        stateName: 'WA',
+        cityName: 'Minneapolis',
+        title: 'We had a great Weekend',
+        imgUrl: 'https://picsum.photos/300',
+        postImage: [
+            'https://picsum.photos/200/300',
+            'https://c2.staticflickr.com/8/7577/28973580825_d8f541ba3f_b.jpg',
+            'https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg',
+        ],
+        location: [44.9738268491232, -93.29340668903093],
+        content: [`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nihil est enim, de quo minus dubitari possit, quam et honesta expetenda per se et eodem modo turpia per se esse fugienda. Ita, quem ad modum in senatu semper est aliquis, qui interpretem postulet, sic, isti nobis cum interprete audiendi sunt. Tanta vis admonitionis inest in locis; Duo Reges: constructio interrete. Indicant pueri, in quibus ut in speculis natura cernitur. Hinc ceteri particulas arripere conati suam quisque videro voluit afferre sententiam. Urgent tamen et nihil remittunt.`]
+    },
+    {
+        date: '04212021',
+        stateName: 'IL',
+        cityName: 'Chicago',
+        title: 'Third in List',
+        imgUrl: 'https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg',
+        postImage: [
+            'https://picsum.photos/200/300',
+            'https://picsum.photos/300/400',
+            'https://picsum.photos/500',
+        ],
+        location: [41.894783463530594, -87.62389130730169],
+        content: [`Ita fit cum gravior, tum etiam splendidior oratio. Huic mori optimum esse propter desperationem sapientiae, illi propter spem vivere.`,
+         `Haeret in salebra. Ita est quoddam commune officium sapientis et insipientis, ex quo efficitur versari in iis, quae media dicamus. Atque ab his initiis profecti omnium virtutum et originem et progressionem persecuti sunt.`,
+          `Quis autem de ipso sapiente aliter existimat, quin, etiam cum decreverit esse moriendum, tamen discessu a suis atque ipsa relinquenda luce moveatur? Graecis hoc modicum est: Leonidas, Epaminondas, tres aliqui aut quattuor; Maximas vero virtutes iacere omnis necesse est voluptate dominante.`]
     }, 
+    {
+        date: '04212021',
+        stateName: 'OH',
+        cityName: 'Toledo',
+        title: 'Family Week!!!',
+        imgUrl: 'https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg',
+        postImage: [
+            'https://picsum.photos/200/300',
+            'https://picsum.photos/300/400',
+            'https://picsum.photos/500',
+        ],
+        location: [41.66496968283615, -83.56222810460461],
+        content: [`Ita fit cum gravior, tum etiam splendidior oratio. Huic mori optimum esse propter desperationem sapientiae, illi propter spem vivere.`,
+         `Haeret in salebra. Ita est quoddam commune officium sapientis et insipientis, ex quo efficitur versari in iis, quae media dicamus. Atque ab his initiis profecti omnium virtutum et originem et progressionem persecuti sunt.`,
+          `Quis autem de ipso sapiente aliter existimat, quin, etiam cum decreverit esse moriendum, tamen discessu a suis atque ipsa relinquenda luce moveatur? Graecis hoc modicum est: Leonidas, Epaminondas, tres aliqui aut quattuor; Maximas vero virtutes iacere omnis necesse est voluptate dominante.`]
+    },
+    {
+        date: '04212021',
+        stateName: 'PA',
+        cityName: 'Pittsburgh',
+        title: 'Family Week!!!',
+        imgUrl: 'https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg',
+        postImage: [
+            'https://picsum.photos/200/300',
+            'https://picsum.photos/300/400',
+            'https://picsum.photos/500',
+        ],
+        location: [40.44436973199181, -79.94854833963926],
+        content: [`Ita fit cum gravior, tum etiam splendidior oratio. Huic mori optimum esse propter desperationem sapientiae, illi propter spem vivere.`,
+         `Haeret in salebra. Ita est quoddam commune officium sapientis et insipientis, ex quo efficitur versari in iis, quae media dicamus. Atque ab his initiis profecti omnium virtutum et originem et progressionem persecuti sunt.`,
+          `Quis autem de ipso sapiente aliter existimat, quin, etiam cum decreverit esse moriendum, tamen discessu a suis atque ipsa relinquenda luce moveatur? Graecis hoc modicum est: Leonidas, Epaminondas, tres aliqui aut quattuor; Maximas vero virtutes iacere omnis necesse est voluptate dominante.`]
+    },
     {
         date: '04212021',
         stateName: 'VT',
@@ -88,6 +163,7 @@ const PostData = [
         location: [ 40.03006879330159, -105.26504071159694],
         content: [`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nihil est enim, de quo minus dubitari possit, quam et honesta expetenda per se et eodem modo turpia per se esse fugienda. Ita, quem ad modum in senatu semper est aliquis, qui interpretem postulet, sic, isti nobis cum interprete audiendi sunt. Tanta vis admonitionis inest in locis; Duo Reges: constructio interrete. Indicant pueri, in quibus ut in speculis natura cernitur. Hinc ceteri particulas arripere conati suam quisque videro voluit afferre sententiam. Urgent tamen et nihil remittunt.`]
     },
+    
     
     
 
