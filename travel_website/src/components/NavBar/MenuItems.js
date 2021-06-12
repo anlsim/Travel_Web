f7d3855c4aea172ad323b552390d2345ed504a68
anlsim <anlsimental@gmail.com>
2021-06-12 15:40:49 -0700

    Responsive NavBar

---
+++ travel_website/src/components/NavBar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: 'BLOG',
+        url: '/blog',
+        cName: 'navBar-menu-links'
+    },
+    {
+        title: 'GALLERY',
+        url: '/gallery',
+        cName: 'navBar-menu-links'
+    },
+    {
+        title: 'MAP',
+        url: '/map',
+        cName: 'navBar-menu-links'
+    },
+    {
+        title: 'ABOUT',
+        url: '/blog',
+        cName: 'navBar-menu-links'
+    },
+    
+]
--- travel_website/src/components/NavBar/NavBar.js
+++ travel_website/src/components/NavBar/NavBar.js
@@ -1,57 +1,58 @@
-import {useContext}from 'react'
+import {useContext, useState}from 'react'
 import {Link} from 'react-router-dom';
 import {Context} from "../../context/Contex";
 import "./NavBar.scss";
-
+import {MenuItems} from './MenuItems'
 
 export default function NavBar() {
   const {user, dispatch } = useContext(Context);
+  const[clicked, setClicked] = useState(false);
   const handleLogOut = () => {
     dispatch({type: "LOGOUT"});
   };
  
+  const handleClicked = () => {
+    setClicked(!clicked);
+  };
 
-  // console.log(user.admin)
   return (
     <>
-    <nav className="navBar navbar-expand-lg navbar-light">
-    
+    <nav className="navBar">
       <Link className="navBar-logo" to="/">TRAVEL BLOG</Link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-      <ul className="navBar-menu"> 
-        <li className="navBar-menu-link">
-          <Link to="/blog">BLOG</Link>
-        </li>
-        <li className="navBar-menu-link">
-          <Link to="/gallery">GALLERY</Link>
-        </li>
-        <li className="navBar-menu-link">
-          <Link to="/map">MAP</Link>
-        </li>         
-        <li className="navBar-menu-link">
-          <Link to="/about">ABOUT</Link>
-        </li>
+      <div className="navBar-icon" onClick={handleClicked}>
+        <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+      </div>
+      <ul className={clicked ? 'navBar-menu active': 'navBar-menu'}> 
+          {
+            MenuItems.map((item, index) => {
+              return(
+                <li key={index}>
+                  <Link className={item.cName} to={item.url}>{item.title}</Link>
+                </li>
+              )
+              
+            })
+          }     
+        
           { user ? (
           <>
             {user.admin &&
-            <li className="navBar-menu-link">
-                <Link to="/publish">PUBLISH</Link>
+            <li>
+                <Link className="navBar-menu-links nav-item" to="/publish">PUBLISH</Link>
             </li>
             }
-            <li className="navBar-menu-link" onClick={handleLogOut}>
-              <Link to="/">{user && "LOGOUT"}</Link>        
+            <li  onClick={handleLogOut}>
+              <Link className="navBar-menu-links nav-item" to="/">{user && "LOGOUT"}</Link>        
             </li>
             </>
             ) : (
-            <li className="navBar-menu-link">
-                <Link to="/">{user && "LOGOUT"}</Link> 
+            <li>
+                <Link className="navBar-menu-links nav-item" to="/">{user && "LOGOUT"}</Link> 
             </li>
             )}
           </ul>
-          </div>
+      
+        
     </nav>
 
 
